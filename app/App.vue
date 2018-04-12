42514f8f39c4b38d8c047b7f7b46b79b10b9201f
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-04-12 09:21:07 +0000

    deploy validator smart contract scenario

--- app/App.vue
+++ app/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div class="page-layout mainScreen">
+    <div class="page-layout">
       <md-toolbar class="md-primary">
         <div class="md-toolbar-row">
           <div class="md-toolbar-section-start">
@@ -16,12 +16,47 @@
               </md-button>
 
               <md-menu-content>
-                <md-menu-item><md-button class="md-primary">Show Your Private Key</md-button></md-menu-item>
+                <md-menu-item><md-button class="md-primary">Balance</md-button></md-menu-item>
               </md-menu-content>
             </md-menu>
           </div>
         </div>
       </md-toolbar>
+      <div class="main-content">
+          <div class="table-container">
+              <md-table md-card>
+                  <md-table-toolbar>
+                      <h1 class="md-title">Candidates</h1>
+                  </md-table-toolbar>
+
+                  <md-table-row>
+                      <md-table-head md-numeric>ID</md-table-head>
+                      <md-table-head>Address</md-table-head>
+                      <md-table-head>Name</md-table-head>
+                      <md-table-head></md-table-head>
+                  </md-table-row>
+              </md-table>
+          </div>
+          <div class="table-container">
+              <md-table md-card>
+                  <md-table-toolbar>
+                      <h1 class="md-title">Validators</h1>
+                  </md-table-toolbar>
+
+                  <md-table-row>
+                      <md-table-head md-numeric>ID</md-table-head>
+                      <md-table-head>Address</md-table-head>
+                      <md-table-head>Name</md-table-head>
+                  </md-table-row>
+
+                  <md-table-row>
+                      <md-table-cell md-numeric>1</md-table-cell>
+                      <md-table-cell>Shawna Dubbin</md-table-cell>
+                      <md-table-cell>sdubbin0@geocities.com</md-table-cell>
+                  </md-table-row>
+              </md-table>
+          </div>
+      </div>
     </div>
   </div>
 </template>
@@ -50,5 +85,13 @@ export default {
 </script>
 
 <style>
+.main-content {
+    width: 960px;
+    margin: 0 auto;
+}
+
+.table-container {
+    padding-top: 40px;
+}
 
 </style>
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,13 @@
+var TomoValidator = artifacts.require('./TomoValidator');
+var config = require('config');
+
+module.exports = function(deployer) {
+    return deployer.deploy(TomoValidator, [
+        '0x89cdd0e4226204ad1a093cf898b6f4b1835b2004',
+        '0xfd7c1fc5ee3a3eb515f07692cef434ee5f694e8c'
+    ], [
+        '1000000000000000000000',
+        '1000000000000000000000'
+    ]).then(() => {
+    });
+};
