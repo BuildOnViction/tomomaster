152f2a3d400a107b6013356d66b5081bb83b3083
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-04-17 10:56:40 +0000

    add details page

--- app/app.js
+++ app/app.js
@@ -1,7 +1,29 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import CandidateView from './components/candidates/View.vue'
+import CandidateList from './components/candidates/List.vue'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/', component: CandidateList
+        },
+        {
+            path: '/candidates', component: CandidateList
+        },
+        {
+            path: '/candidates/:address', component: CandidateView
+        }
+    ]
+});
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    router: router,
+    template: '<App/>',
+    components: { App }
 })
