e57e8d53080220755a8c0d78b3d7b0e713fc4e42
hka <hoangkianh31592@gmail.com>
2018-04-24 12:16:21 +0700

    :bug: remove Warning message "MetaMask: web3 will be deprecated"

--- app/app.js
+++ app/app.js
@@ -8,31 +8,41 @@ import CandidateApply from './components/candidates/Apply.vue'
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.css';
 import 'vue-material/dist/theme/default.css';
+import Web3 from 'web3';
 import { default as contract } from 'truffle-contract';
 import TomoValidatorArtifacts from '../build/contracts/TomoValidator.json';
 Vue.use(VueMaterial)
 
-var web3 = window.web3 || false;
-Vue.prototype.web3 = web3;
-Vue.prototype.TomoValidator = contract(TomoValidatorArtifacts);
-Vue.prototype.TomoValidator.setProvider(Vue.prototype.web3.currentProvider);
-Vue.prototype.getAccount = function() {
-    var p = new Promise(function(resolve, reject) {
-        web3.eth.getAccounts(function(err, accs) {
-            if (err != null) {
-                console.log("There was an error fetching your accounts.");
-                reject(err);
-            }
+if (typeof web3 !== 'undefined') {
+    var web3js = new Web3(web3.currentProvider);
+} else {
+    web3js = false;
+} 
 
-            if (accs.length == 0) {
-                console.log("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-                reject(err);
-            }
+if (web3js instanceof Web3) {
+    Vue.prototype.web3 = web3js;
+    Vue.prototype.TomoValidator = contract(TomoValidatorArtifacts);
+    Vue.prototype.TomoValidator.setProvider(web3js.currentProvider);
+    Vue.prototype.getAccount = function() {
+        var p = new Promise(function(resolve, reject) {
+            web3js.eth.getAccounts(function(err, accs) {
+                if (err != null) {
+                    console.log("There was an error fetching your accounts.");
+                    reject(err);
+                }
 
-            resolve(accs[0]);
+                if (accs.length == 0) {
+                    console.log("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+                    reject(err);
+                }
+
+                resolve(accs[0]);
+            });
         });
-    });
-    return p;
+        return p;
+    }
+} else {
+    Vue.prototype.TomoValidator = contract(TomoValidatorArtifacts);
 }
 
 Vue.use(VueRouter);
