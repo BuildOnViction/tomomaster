2991f6624ef62d76826b0708c326bf6d69f4d223
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-04-27 03:40:01 +0000

    add eslint

--- elect.js
+++ elect.js
@@ -1,30 +1,28 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 
-let win = null;
+let win = null
 
 app.on('ready', function () {
+    // Initialize the window to our specified dimensions
+    win = new BrowserWindow({ width: 1000, height: 600 })
 
-  // Initialize the window to our specified dimensions
-  win = new BrowserWindow({width: 1000, height: 600});
+    // Specify entry point to default entry point of vue.js
+    win.loadURL('http://localhost:3000')
 
-  // Specify entry point to default entry point of vue.js
-  win.loadURL('http://localhost:3000');
-
-  // Remove window once app is closed
-  win.on('closed', function () {
-  win = null;
-  });
-
-});
-//create the application window if the window variable is null
-app.on('activate', () => {
-  if (win === null) {
-  createWindow()
-  }
+    // Remove window once app is closed
+    win.on('closed', function () {
+        win = null
+    })
 })
-//quit the app once closed
+// create the application window if the window variable is null
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+})
+// quit the app once closed
 app.on('window-all-closed', function () {
-  if (process.platform != 'darwin') {
-  app.quit();
-  }
-});
+    if (process.platform != 'darwin') {
+        app.quit()
+    }
+})
