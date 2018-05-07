8b3b3dae5078b573bda350e861a875b8a1d9f867
hka <hoangkianh31592@gmail.com>
2018-05-07 10:07:35 +0700

    remove Retire dialog prompt in the Apply screen

--- app/components/candidates/Apply.vue
+++ app/components/candidates/Apply.vue
@@ -23,10 +23,6 @@
                         v-if="!isCandidate"
                         class="md-primary"
                         @click="applyActive = true;"><md-icon>arrow_upward</md-icon> Apply</md-button>
-                    <md-button
-                        v-if="isCandidate"
-                        class="md-accent"
-                        @click="retireActive = true;"><md-icon>arrow_downward</md-icon> Retire</md-button>
                 </md-card-actions>
             </md-card>
         </div>
@@ -38,12 +34,6 @@
             md-input-placeholder="Type $TOMO..."
             md-confirm-text="Confirm"
             @md-confirm="apply()"/>
-        <md-dialog-confirm
-            :md-active.sync="retireActive"
-            md-title="Do you want to retire?"
-            md-content="If you retire, you will receive back all your deposit."
-            md-confirm-text="Confirm"
-            @md-confirm="retire()"/>
         <md-snackbar
             :md-active.sync="showSnackbar"
             md-position="center"
@@ -61,7 +51,6 @@ export default {
     data () {
         return {
             applyActive: false,
-            retireActive: false,
             showSnackbar: false,
             isCandidate: false,
             applyValue: 10000
