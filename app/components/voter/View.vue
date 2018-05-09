b4a5f1dee70cdda79eaa2fc32bc191ff09505806
hka <hoangkianh31592@gmail.com>
2018-05-09 12:06:55 +0700

    :construction: adding Voter view

--- app/components/voter/View.vue
+++ app/components/voter/View.vue
@@ -8,8 +8,8 @@
                 </md-card-header>
 
                 <md-card-content>
-                    <p>Your balance: <strong>100 $TOMO</strong></p>
-                    <p>You voted: <strong>20 $TOMO</strong></p>
+                    <p>Balance: <strong>{{ balance }} $TOMO</strong></p>
+                    <p>Voted: <strong>{{ totalVoted }} $TOMO</strong></p>
                 </md-card-content>
 
                 <md-table>
@@ -26,12 +26,14 @@
                         <md-table-head/>
                     </md-table-row>
 
-                    <md-table-row>
-                        <md-table-cell md-numeric>1</md-table-cell>
+                    <md-table-row
+                        v-for="(c, key) in candidates"
+                        :key="key">
+                        <md-table-cell md-numeric>{{ key + 1 }}</md-table-cell>
                         <md-table-cell>
-                            0xaaa
+                            <router-link :to="'/candidate/' + c.address">{{ c.address }}</router-link>
                         </md-table-cell>
-                        <md-table-cell>1 $TOMO</md-table-cell>
+                        <md-table-cell>{{ c.cap }} $TOMO</md-table-cell>
                         <md-table-cell>
                             <md-button
                                 class="md-raised md-primary">Vote</md-button>
@@ -49,7 +51,54 @@ export default {
     name: 'App',
     data () {
         return {
-            voter: this.$route.params.address
+            voter: this.$route.params.address,
+            candidates: [],
+            balance: 0,
+            totalVoted: 0
+        }
+    },
+    computed: {
+        sortedCandidates: function () {
+            return this.candidates.slice().sort(function (a, b) {
+                return b.cap - a.cap
+            })
+        }
+    },
+    watch: {},
+    update () {},
+    created: async function () {
+        let self = this
+        try {
+            let voter = self.$route.params.address
+            let account = await self.getAccount()
+            let contract = await self.TomoValidator.deployed()
+            let candidates = await contract.getCandidates.call({ from: account })
+
+            candidates.map(async (candidate) => {
+                let voters = await contract.getVoters.call(candidate, { from: account })
+                // console.log(candidate)
+                // console.log(voters)
+
+                voters.map(async (v) => {
+                    if (v === voter) {
+                        let cap = await contract.getVoterCap.call(candidate, v, { from: account })
+                        self.totalVoted += parseFloat(cap / 10 ** 18)
+                        self.candidates.push({
+                            address: candidate,
+                            cap: (cap / 10 ** 18)
+                        })
+                    }
+                })
+            })
+            // let cap = await contract.getVoterCap.call()
+            self.web3.eth.getBalance(voter, function (a, b) {
+                self.balance = b / 10 ** 18
+                if (a) {
+                    console.log('Got an error', a)
+                }
+            })
+        } catch (e) {
+            console.log(e)
         }
     }
 }
