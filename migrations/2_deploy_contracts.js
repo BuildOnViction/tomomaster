8b0f95067ae88a728a8da351812548a491c48d6f
Minh Khai Do <khaihkd@gmail.com>
2018-06-25 15:33:21 +0700

    remove dotenv, babel package, use npm config & update more test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,15 @@
+const config = require('config');
+
 const TomoValidator = artifacts.require('./TomoValidator');
 const TomoRandomize = artifacts.require('./TomoRandomize');
 const BlockSigner = artifacts.require('./BlockSigner');
 
-const minCandidateCap = process.env.MIN_CANDIDATE_CAP;
-const maxValidatorNumber = process.env.MAX_VALIDATOR_NUMBER;
-const candidateWithdrawDelay = process.env.CANDIDATE_WITHDRAW_DELAY;
-const epochNumber = process.env.EPOCH_NUMBER;
-const blockTimeSecret = process.env.BLOCK_TIME_SECRET;
-const blockTimeOpening = process.env.BLOCK_TIME_OPENING;
+const minCandidateCap = config.get('truffle.minCandidateCap');
+const maxValidatorNumber = config.get('truffle.maxValidatorNumber');
+const candidateWithdrawDelay = config.get('truffle.candidateWithdrawDelay');
+const epochNumber = config.get('truffle.epochNumber');
+const blockTimeSecret = config.get('truffle.blockTimeSecret');
+const blockTimeOpening = config.get('truffle.blockTimeOpening');
 
 
 module.exports = function(deployer) {
--- test/TomoValidatorTest.js
+++ test/TomoValidatorTest.js
@@ -11,6 +11,7 @@ contract('TomoValidator', (accounts) => {
         await validator.propose(accounts[0], nodeUrl, { from : accounts[0], value: 5.0 * 10 ** 18 })
 
         assert.equal((await validator.getCandidates.call()).valueOf()[0], accounts[0])
+        assert.equal((await validator.isCandidate.call(accounts[0])).valueOf(), true)
     })
 
     it('Can not become an candidate when the deposit is less than the minimum ', async () => {
@@ -27,6 +28,8 @@ contract('TomoValidator', (accounts) => {
         await validator.propose(accounts[0], nodeUrl, { from : accounts[0], value: 5.0 * 10 ** 18 })
 
         assert.equal((await validator.getCandidates.call()).valueOf()[0], accounts[0])
+        assert.equal((await validator.isCandidate.call(accounts[0])).valueOf(), true)
+        await tryCatch(validator.propose(accounts[0], nodeUrl, { from : accounts[0], value: 5.0 * 10 ** 18 }), errTypes.revert)
     })
 
     it('Vote a candidate', async () => {
@@ -38,5 +41,48 @@ contract('TomoValidator', (accounts) => {
         await validator.vote(accounts[1], { from: accounts[2], value: 2.0 * 10 ** 18 })
 
         assert.equal((await validator.getVoters.call(accounts[1])).valueOf()[0], accounts[2])
+        assert.equal((await validator.getVoterCap.call(accounts[1], accounts[2])).valueOf(), 2.0 * 10 ** 18)
+    })
+
+    it('UnVote a candidate', async () => {
+        const validator = await TomoValidator.new([], [], 5, 99, 100)
+
+        await validator.propose(accounts[1], nodeUrl, { from : accounts[0], value: 5.0 * 10 ** 18 })
+        assert.equal((await validator.getCandidates.call()).valueOf()[0], accounts[1])
+
+        await validator.vote(accounts[1], { from: accounts[2], value: 2.0 * 10 ** 18 })
+
+        assert.equal((await validator.getVoters.call(accounts[1])).valueOf()[0], accounts[2])
+        assert.equal((await validator.getVoterCap.call(accounts[1], accounts[2])).valueOf(), 2.0 * 10 ** 18)
+
+        // Cannot unvote if cap is great than amount user vote
+        await tryCatch(validator.unvote(accounts[1], 3.0 * 10 ** 18, { from: accounts[2] }), errTypes.revert)
+
+        await validator.unvote(accounts[1], 1.0 * 10 ** 18, { from: accounts[2] })
+        assert.equal((await validator.getVoterCap.call(accounts[1], accounts[2])).valueOf(), 1.0 * 10 ** 18)
+    })
+
+    it('Update NodeUrl of a candidate', async () => {
+        const validator = await TomoValidator.new([], [], 5, 99, 100)
+        let nurl = 'node url for test'
+        await validator.propose(accounts[0], nurl, { from : accounts[0], value: 5.0 * 10 ** 18 })
+
+        assert.equal((await validator.isCandidate.call(accounts[0])).valueOf(), true)
+        assert.equal((await validator.getCandidateNodeUrl.call(accounts[0])).valueOf(), nurl)
+
+        await validator.setNodeUrl(accounts[0], nodeUrl, { from : accounts[0] })
+        assert.equal((await validator.getCandidateNodeUrl.call(accounts[0])).valueOf(), nodeUrl)
+    })
+
+    it('A candidate resign ', async () => {
+        const validator = await TomoValidator.new([], [], 5, 99, 0)
+        await validator.propose(accounts[0], nodeUrl, { from : accounts[0], value: 5.0 * 10 ** 18 })
+
+        assert.equal((await validator.isCandidate.call(accounts[0])).valueOf(), true)
+
+        await validator.resign(accounts[0], { from : accounts[0] })
+        assert.equal((await validator.isCandidate.call(accounts[0])).valueOf(), false)
+
+        // await validator.withdraw(accounts[0], { from : accounts[0] })
     })
 })
