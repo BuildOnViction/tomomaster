ab5e99c5f01cbce87ece50f4d78f3d63931ab010
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-07-12 07:21:06 +0000

    add 3 seeds masternode

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -16,7 +16,18 @@ if (config.has('truffle')){
 
 module.exports = function(deployer) {
 
-    return deployer.deploy(TomoValidator, [], [], null, minCandidateCap, maxValidatorNumber, candidateWithdrawDelay, voterWithdrawDelay).then((tv) => {
+    let candidates = [
+        '0xf99805B536609cC03AcBB2604dFaC11E9E54a448',
+        '0x31b249fE6F267aa2396Eb2DC36E9c79351d97Ec5',
+        '0xfC5571921c6d3672e13B58EA23DEA534f2b35fA0'
+    ]
+    let caps = [
+        '50000000000000000000000',
+        '50000000000000000000000',
+        '50000000000000000000000'
+    ]
+    let firstOwner = '0x487d62d33467c4842c5e54Eb370837E4E88BBA0F'
+    return deployer.deploy(TomoValidator, candidates, caps, firstOwner, minCandidateCap, maxValidatorNumber, candidateWithdrawDelay, voterWithdrawDelay).then((tv) => {
         return  deployer.deploy(TomoRandomize, epochNumber, blockTimeSecret, blockTimeOpening)
     }). then(() => {
         return deployer.deploy(BlockSigner, epochNumber);
