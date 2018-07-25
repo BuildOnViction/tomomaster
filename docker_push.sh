99d931e0dc1428ae4099454634589c3ebb134cf2
etienne-napoleone <etienne@tomochain.com>
2018-07-25 18:19:09 +0700

    Refactor docker push script + add pipeline for tagged commits

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-docker tag tomochain/tomomaster tomochain/tomomaster:latest
-docker tag tomochain/tomomaster tomochain/tomomaster:$TRAVIS_BUILD_ID
-docker push tomochain/tomomaster:latest
-docker push tomochain/tomomaster:$TRAVIS_BUILD_ID
+docker tag tomochain/tomomaster tomochain/tomomaster:$1
+docker push tomochain/tomomaster:$1
