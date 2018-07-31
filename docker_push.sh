57a41bb6dec770a35c47f49a266076888a1fe831
etienne-napoleone <etienne@tomochain.com>
2018-07-31 17:36:54 +0700

    Clean files

--- docker_push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-docker tag tomochain/tomomaster tomochain/tomomaster:$1
-docker push tomochain/tomomaster:$1
