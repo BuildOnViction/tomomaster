aff6e910d79763f3b3af6bacb3bb233c5990b990
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-08-09 11:05:10 +0000

    count signers list

--- queues/reward.js
+++ queues/reward.js
@@ -1,16 +1,32 @@
 'use strict'
 
 /*
-const db = require('../models/mongodb')
-const config = require('config')
 const web3 = require('../models/blockchain/chain')
 */
+const db = require('../models/mongodb')
+const config = require('config')
 const consumer = {}
 
 consumer.name = 'reward'
 consumer.task = async function (job, done) {
     let block = job.data.block
-    console.log('reward for', block.hash)
+    let epoch = parseInt(config.get('blockchain.epoch'))
+    let blockNumber = parseInt(block.number)
+
+    if (blockNumber % epoch !== 0) {
+        return done()
+    }
+
+    /*
+    let rewardEpochNumber = (blockNumber / epoch) - 1
+    let rewardBlockNumber = blockNumber - epoch
+    */
+    let sn = await db.Signer.findOne({
+        blockNumber: blockNumber - (2 * epoch)
+    })
+
+    let signers = (sn || {}).signers || []
+    console.log('checkpoint !!!', signers)
 
     done()
 }
