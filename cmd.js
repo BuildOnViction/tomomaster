656d17faf215175b4ac315028a238f495895f1dd
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-08-19 07:25:54 +0000

    update nodeId from cmd

--- cmd.js
+++ cmd.js
@@ -2,6 +2,7 @@
 
 const commander = require('commander')
 const db = require('./models/mongodb')
+const _ = require('lodash')
 
 commander
     .version('0.1.0')
@@ -12,27 +13,31 @@ commander
     .alias('l')
     .description('List candidates')
     .action(() => {
-        return db.Candidate.find().select(['candidate', 'name'])
+        return db.Candidate.find().select(['candidate', 'name', 'nodeId'])
             .then(candidates => {
                 candidates.forEach(c => {
-                    console.log('id:', c.candidate, 'name:', c.name || 'null')
+                    console.log('id:', c.candidate,
+                        'name:', c.name || 'null',
+                        'nodeId:', c.nodeId || 'null')
                 })
                 process.exit()
             })
     })
 
 commander
-    .command('update <id> <name>')
+    .command('update <id>')
     .alias('u')
     .description('Update candidate information')
-    .action(async (id, name) => {
-        await db.Candidate.update({
+    .option('-n, --name <name>', 'Name of Candidate')
+    .option('-i, --nodeId <nodeId>', 'nodeId of Candidate')
+    .action(async (id, options) => {
+        let set = _.pick(options, ['name', 'nodeId'])
+        let u = await db.Candidate.update({
             candidate: id
         }, {
-            $set: {
-                name: name
-            }
+            $set: set
         })
+        console.log(u)
         process.exit()
     })
 
