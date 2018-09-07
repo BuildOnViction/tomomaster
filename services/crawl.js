bd279a54e9baef25b3684d9541d6ce01c548cc73
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-09-07 09:20:55 +0000

    refactor update signers list

--- services/crawl.js
+++ services/crawl.js
@@ -132,21 +132,8 @@ async function watch () {
             if (err) {
                 return false
             }
-            let blk = chain.eth.getBlock('latest')
-            let buff = Buffer.from(blk.extraData.substring(2), 'hex')
-            let sbuff = buff.slice(32, buff.length - 65)
-            let signers = []
-            if (sbuff.length > 0) {
-                for (let i = 1; i <= sbuff.length / 20; i++) {
-                    let address = sbuff.slice((i - 1) * 20, i * 20)
-                    signers.push('0x' + address.toString('hex'))
-                }
-                db.Signer.create({
-                    networkId: config.get('blockchain.networkId'),
-                    blockNumber: blk.number,
-                    signers: signers
-                })
-            }
+            let blk = await chain.eth.getBlock('latest')
+            await updateSigners(blk)
             q.create('reward', { block: blk })
                 .priority('low').removeOnComplete(true).save()
         })
@@ -157,6 +144,33 @@ async function watch () {
         console.log('Stop process ...')
         process.exit(1)
     }
+}
+
+async function updateSigners (blk) {
+    if (!blk) {
+        let latestBlockNumber = await chain.eth.blockNumber
+        let lastCheckpoint = latestBlockNumber - (latestBlockNumber % parseInt(config.get('blockchain.epoch')))
+        if (lastCheckpoint > 0) {
+            blk = await chain.eth.getBlock(lastCheckpoint)
+        } else {
+            return false
+        }
+    }
+    let buff = Buffer.from(blk.extraData.substring(2), 'hex')
+    let sbuff = buff.slice(32, buff.length - 65)
+    let signers = []
+    if (sbuff.length > 0) {
+        for (let i = 1; i <= sbuff.length / 20; i++) {
+            let address = sbuff.slice((i - 1) * 20, i * 20)
+            signers.push('0x' + address.toString('hex'))
+        }
+        await db.Signer.create({
+            networkId: config.get('blockchain.networkId'),
+            blockNumber: blk.number,
+            signers: signers
+        })
+    }
+    return signers
 }
 
 async function updateCandidateInfo (candidate) {
@@ -243,6 +257,7 @@ async function getCurrentCandidates () {
 try {
     getCurrentCandidates()
     watch()
+    updateSigners(false)
 } catch (e) {
     console.error(e)
 }
