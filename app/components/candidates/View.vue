339da8c5dfe2b55de5134d84afbea18daedf01ee
Vu Pham <pqv199x@hotmail.com>
2018-11-01 13:46:56 +0700

    remove reason column

--- app/components/candidates/View.vue
+++ app/components/candidates/View.vue
@@ -429,11 +429,6 @@ export default {
                     sortable: false
                 },
                 {
-                    key: 'reason',
-                    label: 'Reason',
-                    sortable: false
-                },
-                {
                     key: 'reward',
                     label: 'Reward',
                     sortable: false
@@ -612,16 +607,13 @@ export default {
             let mnRewards = await axios.get(`/api/candidates/${address}/${self.candidate.owner}/getRewards`)
 
             mnRewards.data.map((r) => {
-                if (r.reason === 'MasterNode') {
-                        self.mnRewards.push({
-                        epoch: r.epoch,
-                        signNumber: r.signNumber,
-                        reason: r.reason,
-                        reward: new BigNumber(r.reward).div(1e+18).toFixed(8),
-                        createdAt: moment(r.createdAt).fromNow(),
-                        dateTooltip: moment(r.createdAt).format('lll')
-                    })
-                }
+                    self.mnRewards.push({
+                    epoch: r.epoch,
+                    signNumber: r.signNumber,
+                    reward: new BigNumber(r.reward).div(1e+18).toFixed(8),
+                    createdAt: moment(r.createdAt).fromNow(),
+                    dateTooltip: moment(r.createdAt).format('lll')
+                })
             })
 
             self.recentReward = (self.mnRewards[0] || {}).reward || 0
