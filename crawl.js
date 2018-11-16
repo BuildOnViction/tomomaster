e538b7c572dbb943da0911e16e2d9ac69f5c4cc0
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-11-16 10:51:07 +0700

    add blockNumber when insert Tx

--- crawl.js
+++ crawl.js
@@ -53,6 +53,7 @@ async function watchValidator () {
                         owner: owner,
                         candidate: candidate,
                         capacity: capacity,
+                        blockNumber: blockNumber,
                         createdAt: createdAt
                     })
                     tx.save()
