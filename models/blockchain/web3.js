cb85f6a033dbfebef82e5e91a08af7ca6ed853ab
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-11-27 02:16:33 +0000

    reconnect websocket if close

--- models/blockchain/web3.js
+++ models/blockchain/web3.js
@@ -3,12 +3,20 @@
 const Web3 = require('web3')
 const config = require('config')
 
-const provider = new Web3.providers.WebsocketProvider(config.get('blockchain.ws'))
+let provider = new Web3.providers.WebsocketProvider(config.get('blockchain.ws'))
 const web3 = new Web3(provider)
 
-process.on('uncaughtException', function (err) {
-    console.log('Caught exception: ' + err)
-    process.exit()
+provider.on('error', e => console.log('WS Error', e))
+provider.on('end', e => {
+    console.log('WS closed')
+    console.log('Attempting to reconnect...')
+    provider = new Web3.providers.WebsocketProvider(config.get('blockchain.ws'))
+
+    provider.on('connect', function () {
+        console.log('WSS Reconnected')
+    })
+
+    web3.setProvider(provider)
 })
 
 module.exports = web3
