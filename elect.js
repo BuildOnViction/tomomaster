c337706240df5209de948241447e4175375fd8ba
Vu Pham <pqv199x@hotmail.com>
2018-12-03 13:52:32 +0700

    check

--- elect.js
+++ elect.js
@@ -11,10 +11,12 @@ function createWindow () {
         }
     )
 
-    win.maximize()
+    isOnline(() => {
+        win.maximize()
 
-    // Specify entry point to default entry point of vue.js
-    win.loadURL('https://master.testnet.tomochain.com')
+        // Specify entry point to default entry point of vue.js
+        win.loadURL('https://master.testnet.tomochain.com')
+    })
 
     // Remove window once app is closed
     win.on('closed', function () {
@@ -35,3 +37,41 @@ app.on('window-all-closed', function () {
         app.quit()
     }
 })
+
+function isOnline (callback) {
+    /**
+     * Show a warning to the user.
+     * You can retry in the dialog until a internet connection
+     * is active.
+     */
+    var message = function () {
+        const { dialog } = require('electron').remote
+
+        return dialog.showMessageBox({
+            title:"There's no internet",
+            message:'No internet available, do you want to try again?',
+            type:'warning',
+            buttons:['Try again please","I don\'t want to work anyway'],
+            defaultId: 0
+        }, function (index) {
+            // if clicked "Try again please"
+            if (index === 0) {
+                execute()
+            }
+        })
+    }
+
+    var execute = function () {
+        if (navigator.onLine) {
+            // Execute action if internet available.
+            callback()
+        } else {
+            // Show warning to user
+            // And "retry" to connect
+            message()
+        }
+    }
+
+    // Verify for first time
+    execute()
+}
