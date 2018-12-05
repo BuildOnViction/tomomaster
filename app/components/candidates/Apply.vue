160f330db92e85da822ca8d255c1c4142dc53cf8
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-12-05 11:00:21 +0700

    Update Apply.vue

--- app/components/candidates/Apply.vue
+++ app/components/candidates/Apply.vue
@@ -235,7 +235,7 @@ export default {
                 }, 2000)
             } catch (e) {
                 self.loading = false
-                self.$toasted.show('An error occurred while applying, please try again', {
+                self.$toasted.show(`An error occurred while applying, please fix it and try again: ${String(e)}`, {
                     type: 'error'
                 })
                 console.log(e)
