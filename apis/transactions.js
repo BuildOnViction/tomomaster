3e013fe117ade7814c1fde3ad7086d52955c2bb8
Vu Pham <pqv199x@hotmail.com>
2019-01-15 16:36:08 +0700

    add resign txes to voter tx table

--- apis/transactions.js
+++ apis/transactions.js
@@ -24,8 +24,16 @@ router.get('/voter/:voter', async function (req, res, next) {
     }
     try {
         let txs = await db.Transaction.find({
-            smartContractAddress: config.get('blockchain.validatorAddress'),
-            voter: (req.params.voter || '').toLowerCase()
+            $or: [
+                {
+                    smartContractAddress: config.get('blockchain.validatorAddress'),
+                    voter: (req.params.voter || '').toLowerCase()
+                },
+                {
+                    smartContractAddress: config.get('blockchain.validatorAddress'),
+                    owner: (req.params.voter || '').toLowerCase()
+                }
+            ]
         }).sort({ createdAt: -1 }).limit(limit).skip(skip)
         return res.json(txs)
     } catch (e) {
