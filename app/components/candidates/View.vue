fc10fdf068a6c66244c6c55141eb060b79384b1f
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2019-02-14 14:26:38 +0700

    Update View.vue

--- app/components/candidates/View.vue
+++ app/components/candidates/View.vue
@@ -187,7 +187,7 @@
                             class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 m-xl-0 tomo-info">
                             <p class="tomo-info__title">
                                 <i class="tm-dot tomo-info__icon" />
-                                <span class="tomo-info__text">Slash history</span>
+                                <span class="tomo-info__text">Slashed history</span>
                             </p>
                             <p
                                 id="tomo-info__description--slashed"
