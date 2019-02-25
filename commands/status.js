b93d2bbe0ea51a7e434c41bdeecf74d3d55d09a4
Vu Pham <pqv199x@hotmail.com>
2019-02-25 18:05:40 +0700

    update status command

--- commands/status.js
+++ commands/status.js
@@ -4,54 +4,62 @@
 const web3Rpc = require('../models/blockchain/web3rpc')
 const logger = require('../helpers/logger')
 const config = require('config')
-const moment = require('moment')
+// const moment = require('moment')
 const db = require('../models/mongodb')
 
 async function updateStatues () {
-    // get latest epoch
-    const latestBlock = await web3Rpc.eth.getBlockNumber()
-    const latestEpoch = parseInt(latestBlock / config.get('blockchain.epoch')) - 1
-    // loop from 0 to latest epoch
-    for (let i = 0; i < latestEpoch; i++) {
-        await db.Candidate.updateMany({ status: { $nin: ['RESIGNED'] } }, { $set: { status: 'PROPOSED' } })
-        const blockNumber = (i + 1) * config.get('blockchain.epoch')
-        console.log('Reading block ', blockNumber)
-        const blk = await web3Rpc.eth.getBlock(blockNumber)
+    try {
+        // get latest epoch
+        const latestBlock = await web3Rpc.eth.getBlockNumber()
+        const latestEpoch = parseInt(latestBlock / config.get('blockchain.epoch')) - 1
+        // loop from 0 to latest epoch
+        for (let i = 0; i < latestEpoch; i++) {
+            await db.Candidate.updateMany({ status: { $nin: ['RESIGNED'] } }, { $set: { status: 'PROPOSED' } })
 
-        // update candidate table
-        let buff = Buffer.from(blk.extraData.substring(2), 'hex')
-        let sbuff = buff.slice(32, buff.length - 65)
-        let address
-        if (sbuff.length > 0) {
-            for (let i = 1; i <= sbuff.length / 20; i++) {
-                address = sbuff.slice((i - 1) * 20, i * 20)
-                // update candidate status
-                await db.Candidate.updateOne({
-                    smartContractAddress: config.get('blockchain.validatorAddress'),
-                    candidate: '0x' + address.toString('hex').toLowerCase()
-                }, {
-                    $set: {
-                        status: 'MASTERNODE'
-                    }
-                }, { upsert: true })
+            const blockNumber = (i + 1) * config.get('blockchain.epoch')
+            console.log('Reading block ', blockNumber)
+            const blk = await web3Rpc.eth.getBlock(blockNumber)
+
+            // update candidate table
+            let buff = Buffer.from(blk.extraData.substring(2), 'hex')
+            let sbuff = buff.slice(32, buff.length - 65)
+            let address
+            if (sbuff.length > 0) {
+                for (let i = 1; i <= sbuff.length / 20; i++) {
+                    address = sbuff.slice((i - 1) * 20, i * 20)
+                    // update candidate status
+                    await db.Candidate.updateOne({
+                        smartContractAddress: config.get('blockchain.validatorAddress'),
+                        candidate: '0x' + address.toString('hex').toLowerCase()
+                    }, {
+                        $set: {
+                            status: 'MASTERNODE'
+                        }
+                    }, { upsert: true })
+                }
             }
+            console.log('Done update candidate infor')
+
+            // update penalties
+            // check last epoch to see if masternode is slashed
+            await updatePenalty(blk)
+            const epoch = parseInt(blockNumber / config.get('blockchain.epoch')) - 1
+            if (epoch === 615) {
+                const c = await db.Candidate.find({ status: 'PROPOSED' })
+                console.log('checking', c)
+            }
+            console.log('Done update candidate penalty')
+            await updateStatusHistory(blockNumber)
+            console.log('Done update status')
         }
-        console.log('Done update candidate infor')
-
-        // update penalties
-        await updatePenalty(blk)
-        console.log('Done update candidate penalty')
-        await updateStatusHistory(blockNumber)
-        console.log('Done update status')
+    } catch (error) {
+        console.log(error)
     }
-
-    // update candidate status
-    // got all status
-    // updateStatusHistory
 }
 
 async function updateStatusHistory (blk) {
     try {
+        console.log('is this run first????')
         const penalties = []
         const masternodes = []
         const proposes = []
@@ -74,13 +82,17 @@ async function updateStatusHistory (blk) {
         propose.map(p => proposes.push(p.candidate))
         const blockData = await blockDataPromise
         // insert Status table
+        if (epoch === 615) {
+            const c = await db.Candidate.find({ status: 'SLASHED' })
+            console.log('history', c)
+        }
 
         await db.Status.findOneAndUpdate({ epoch: epoch }, {
             epoch: epoch,
             masternodes: masternodes,
             penalties: penalties,
             proposes: proposes,
-            createdAt: moment.unix(blockData.timestamp).utc()
+            createdAt: new Date(blockData.timestamp * 1000)
         }, { upsert: true })
     } catch (e) {
         logger.error('updateStatusHistory %s', e)
@@ -105,6 +117,36 @@ async function updatePenalty (blk) {
                 }
             }, { upsert: true })
             // await updateCandidateSlashed('0x' + address.toString('hex').toLowerCase(), blk.number)
+        }
+    } else {
+        const currentEpoch = (blk.number / config.get('blockchain.epoch')) - 1
+
+        const lastEpoch = currentEpoch - 1
+        const lastEpochData = await db.Status.findOne({ epoch: lastEpoch })
+        if (lastEpoch === 615) {
+            console.log(`
+            
+            
+            HERERERER
+            currrent: ${currentEpoch}
+            last: ${lastEpoch}
+            
+            ${lastEpochData}`)
+        }
+        if (lastEpochData) {
+            console.log(11111111111111)
+            console.log(currentEpoch)
+            // update candidate again
+            for (let i = 0; i < lastEpochData.penalties.length; i++) {
+                await db.Candidate.updateOne({
+                    smartContractAddress: config.get('blockchain.validatorAddress'),
+                    candidate: lastEpochData.penalties[i].toLowerCase()
+                }, {
+                    $set: {
+                        status: 'SLASHED'
+                    }
+                }, { upsert: true })
+            }
         }
     }
 }
