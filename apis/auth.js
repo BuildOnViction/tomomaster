8422cc95008bfe81cf7872a4a4441e415b9fcc81
pqv199x <pqv199x@hotmail.com>
2020-07-03 10:57:00 +0700

    fix login via qr code issue

--- apis/auth.js
+++ apis/auth.js
@@ -99,7 +99,9 @@ function ecRecover (message, signature) {
     const signatureBuffer = utils.toBuffer(signature)
     const signatureParams = utils.fromRpcSig(signatureBuffer)
 
-    const m = utils.toBuffer(message)
+    const buffer = Buffer.from(message)
+    const msgBuffer = '0x' + buffer.toString('hex')
+    const m = utils.toBuffer(msgBuffer)
     const msgHash = utils.hashPersonalMessage(m)
 
     const publicKey = utils.ecrecover(
