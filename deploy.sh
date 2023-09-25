a55c62ecb1d7c8aa03236b7a50d56ff829dc9e40
Bob Jr <nguyennt@coin98.finance>
2023-09-25 09:33:26 +0700

    [release] Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,7 @@ npm config set user 0
 npm config set unsafe-perm true
 
 
-mkdir node_modules
-chmod 777 -R node_modules/
-npm install
 cp ../config/* ./config/
 
-pm2 reload crawler
-pm2 reload tomomaster
+pm2 list || npm i -g pm2
+pm2 startOrReload pm2.config.js
